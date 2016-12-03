075b9fb0db911b03e220d9b6d9e320def67fcac6
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-03 12:44:32 -0800

    buffers

--- F6_Buffers/Starter/app.js
+++ F6_Buffers/Starter/app.js
@@ -4,3 +4,11 @@ var buf = new Buffer('Hello', 'utf8');
 console.log(buf);
 console.log(buf.toString());
 console.log(buf.toJSON());
+buf.write('wo');
+console.log(buf.toString());
+
+buf.write('ezr');
+console.log(buf.toString());
+
+buf.write('hel');
+console.log(buf.toString());
