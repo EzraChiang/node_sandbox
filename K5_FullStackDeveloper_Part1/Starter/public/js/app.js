cc1be9386b68736d84936fab798a6ea8cc30194f
Ezra Chiang <ezrachiang@Ezras-MacBook-Pro.local>
2016-12-06 15:47:59 -0800

    Working with Server side JS vs Client Side JS

---
+++ K5_FullStackDeveloper_Part1/Starter/public/js/app.js
@@ -0,0 +1,20 @@
+angular.module('TestApp', []);
+
+angular.module('TestApp')
+	.controller('MainController', ctrlFunc);
+
+function ctrlFunc() {
+	
+
+	this.people = [
+		{
+			name: 'John Doe'
+		},
+		{
+			name: 'Jane Doe'
+		},
+		{
+			name: 'Jim Doe'
+		}
+	]
+}
